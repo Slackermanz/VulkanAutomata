aae8e6375257a22c7994e9ca122cdd0cbb0e109d
Slackermanz <benkraakman@mailbox.org>
2021-05-26 20:31:20 +1200

    Clean Headless Engine initial upload

--- buildrun.sh
+++ buildrun.sh
@@ -9,8 +9,6 @@
 	./glslc -O ./res/vert/vert_TriQuad.vert -o ./app/vert_TriQuad.spv
 	echo "  ./glslc -O ./dat/frag/frag_automata0000.frag -o ./app/frag_automata0000.spv"
 	./glslc -O ./res/frag/frag_automata0000.frag -o ./app/frag_automata0000.spv
-	echo "  ./glslc -O ./dat/frag/ParamUpdate.frag -o ./app/ParamUpdate.spv"
-	./glslc -O ./res/frag/ParamUpdate.frag -o ./app/ParamUpdate.spv
 	echo ""
 	echo "  g++ VulkanAutomata.cpp -fconcepts -lX11 -L/home/pseudo/vulkan-sdk/1.2.170.0/x86_64/lib/ -lvulkan -o ./app/RunVkAuto"
 	echo ""
